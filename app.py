125b864bf097203141fb43a94f24938310c994ee
Josh Horner <hornerj@patrickind.com>
2022-12-16 11:21:17 -0500

    Initial publish

---
+++ app.py
@@ -0,0 +1,27 @@
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+import certifi
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient('mongodb+srv://padmin:l0ganr04@josh-portfolio.xykc408.mongodb.net/test', tlsCAFile=certifi.where())
+    app.db = client.portfolio
+
+    @app.route('/', methods=['GET', 'POST'])
+    def home():
+        if request.method == 'POST':
+            entry_content = request.form.get('content')
+            formatted_date = datetime.datetime.today().strftime('%Y-%m-%d')
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [
+            (
+                entry['content'], 
+                entry['date'], 
+                datetime.datetime.strptime(entry['date'], '%Y-%m-%d').strftime('%b %d')
+            )
+            for entry in app.db.entries.find({})
+        ]
+        return render_template('home.html', entries=entries_with_date)
+    return app
