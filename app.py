075234ffe4624d4c4ab6bd4e8bb712f9094501bf
Josh Horner <hornerj@patrickind.com>
2022-12-16 14:04:10 -0500

    Adding files for environment variables

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import os
 import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+from dotenv import load_dotenv
 # import certifi
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     # client = MongoClient('mongodb+srv://padmin:l0ganr04@josh-portfolio.xykc408.mongodb.net/test', tlsCAFile=certifi.where())
-    client = MongoClient('mongodb+srv://padmin:l0ganr04@josh-portfolio.xykc408.mongodb.net/test')
+    client = MongoClient(os.environ.get('MONGODB_URI'))
     app.db = client.portfolio
 
     @app.route('/', methods=['GET', 'POST'])
